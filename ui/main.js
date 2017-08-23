609184ed4859313dcf861d193698d92ee37637c5
Ayushksingh <30658332+Ayushksingh@users.noreply.github.com>
2017-08-23 17:33:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,8 +4,15 @@ console.log('Loaded!');
  var element = document.getElementById('main-text');
  element.innerHTML = 'New Value';
   
+  
   //move the image
   var img = document.getElementById('Ayush');
+  var marginLeft = 0;
+  function moveRight() {
+      marginLeft = marginLeft + 10;
+      img.style.marginLeft = marginLeft + 'px';
+  }
   img.onclick = function () {
-      img.style.marginLeft = '100px';
+      var interval =setInterval(moveRight, 100)
+      
   };
